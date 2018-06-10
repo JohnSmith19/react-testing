562a12b3e9b874f0e7126ff14fc991455c0868aa
John Smith <unknown.prop@gmail.com>
2018-06-10 11:54:59 +0900

    Showing Components in the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
 
 const App = () => {
-  return <div>I'm the app component</div>;
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
 };
 
 export default App;

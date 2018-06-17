1ba9a652556464b462a42e61ba2fda6bde797e2e
John Smith <unknown.prop@gmail.com>
2018-06-17 20:10:30 +0900

    Simulating Change Events

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,23 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
 
-it("has a text area and a button", () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+it("has a text area that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" }
+  });
 });

5b14c59d6ac23470458a29b4ac0bfde16e8168cf
John Smith <unknown.prop@gmail.com>
2018-06-17 21:37:41 +0900

    Form submit test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -27,3 +27,18 @@ it("has a text area that users can type in", () => {
   //http://airbnb.io/enzyme/docs/api/ReactWrapper/prop.html
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
+
+it("when form is submitted, text area gets emptied", () => {
+  // input inital value for test
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" }
+  });
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});

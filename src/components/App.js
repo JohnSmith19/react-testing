77670bb029192dea37e181649c13e3a12788e36c
John Smith <unknown.prop@gmail.com>
2018-06-19 21:44:10 +0900

    Rendering a Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-const App = () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {}
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  reunder() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
 
 export default App;

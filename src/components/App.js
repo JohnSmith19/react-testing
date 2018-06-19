f9efb4546aa748fe12ae33f8eb77684e314024e0
John Smith <unknown.prop@gmail.com>
2018-06-19 22:12:04 +0900

    Wiring Up State

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
 class App extends Component {
-  renderButton() {}
+  renderButton() {
+    if (this.props.auth) {
+      return <button>Sign Out</button>;
+    } else {
+      return <button>Sign In</button>;
+    }
+  }
+
   renderHeader() {
     return (
       <ul>
@@ -19,9 +27,10 @@ class App extends Component {
     );
   }
 
-  reunder() {
+  render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
@@ -29,4 +38,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
